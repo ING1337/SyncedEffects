b1c6990a64f404ccc81be08619888bce7465dc86
ING1337 <rubberduckish@gmx.de>
2014-03-11 18:40:05 +0100

    Create demoServer.lua

---
+++ server/demoServer.lua
@@ -0,0 +1,22 @@
+-- EffectGun Demo by ING
+
+MouseTrigger = function(args)
+	if args.button == 1 then
+		-- create a new synced effect
+		lastEffectID = syncedEffects:CreateEffect(args.id, args.raycast.position, nil, 10, 1024, Vector3(0, 2, 0), nil)
+	else
+		-- update the last effect position
+		syncedEffects:UpdateEffect(lastEffectID, args.raycast.position)
+	end
+end
+
+PlayerSpawn = function(args)
+	args.player:SetPosition(Vector3(-10290, 203, -3030)) -- it's funnier to test here...
+	return false
+end
+
+-- ####################################################################################################################################
+
+lastEffectID = 0
+Network:Subscribe("MouseTrigger", MouseTrigger)
+Event:Subscribe("PlayerSpawn", PlayerSpawn)
